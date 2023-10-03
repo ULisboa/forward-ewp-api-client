d0ea53ec8bd99aa31de0663e9e14fa10e14f2785
Fábio Ferreira <fabioferreira@campus.ul.pt>
2023-10-03 14:40:53 +0100

    improvement: add expiration time to JWT tokens (defaults to 30s expiration time in future)

--- src/main/java/pt/ulisboa/forward/ewp/api/client/interceptor/JwtRequestInterceptor.java
+++ src/main/java/pt/ulisboa/forward/ewp/api/client/interceptor/JwtRequestInterceptor.java
@@ -4,6 +4,13 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.temporal.TemporalAmount;
+import java.util.Date;
 import pt.ulisboa.forward.ewp.api.client.config.ClientConfiguration;
 
 public class JwtRequestInterceptor implements RequestInterceptor {
@@ -11,12 +18,16 @@ public class JwtRequestInterceptor implements RequestInterceptor {
   public static final String HEADER_AUTHORIZATION = "Authorization";
   public static final String HEADER_AUTHORIZATION_BEARER_PREFIX = "Bearer ";
 
+  private static final TemporalAmount TOKEN_EXPIRATION_OFFSET = Duration.ofSeconds(30L);
+
   @Override
   public void apply(RequestTemplate requestTemplate) {
     ClientConfiguration configuration = ClientConfiguration.getInstance();
+    Date expiresAt = Date.from(ZonedDateTime.now().plus(TOKEN_EXPIRATION_OFFSET).toInstant());
     String jwtToken =
         JWT.create()
             .withIssuer(configuration.getClientId())
+            .withExpiresAt(expiresAt)
             .sign(Algorithm.HMAC256(configuration.getSecret()));
     requestTemplate.header(HEADER_AUTHORIZATION, HEADER_AUTHORIZATION_BEARER_PREFIX + jwtToken);
   }
